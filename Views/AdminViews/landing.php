0fd425a52ae1dccbf86ff6d01039bc7a771b3a54
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-04-07 13:09:58 -0400

    Added some styling to landing page for admins, not fluid or any scaffolding because I dont feel like changing li's to divs and restructuring the page

--- Views/AdminViews/landing.php
+++ Views/AdminViews/landing.php
@@ -7,45 +7,51 @@
 <div class="landingAdmin">
 	<h1>Administrative Console</h1>
 
-  <p>
+  <div>
     This page provides a portal to administrating content and databases.
     If you add a new admin module you will have to make an entry on this page 
     in order to be able to navigate to it without knowing the url. If you
     make a new module please remember that not everyone is a coder, so always
     provide an admin link to your tool here.
-  </p>
+  </div>
 
   <ul>
     <li>
       <h2>News</h2>
-      <p>
-        Create,edit and remove news stories. Upload images and write new content.
-      </p>
-      <a href="/Admin/?news=new">Manage News</a>
+      <a href="<?= BASEDIR ?>Admin/?news=new">
+        <p>
+          Create,edit and remove news stories. Upload images and write new content.
+        </p>
+        <h3>Manage News</h3>
+      </a>
     </li>
     <li>
+      
       <h2>Hours</h2>
-      <p>
-        Update,Add, and remove Volunteer Help  Hours for active CS Crew Members
-      </p>
-      <a href="/Admin/?hours=display">Manage Hours</a>
+      <a href="<?= BASEDIR ?>Admin/?hours=display">
+        <p>
+          Update,Add, and remove Volunteer Help  Hours for active CS Crew Members
+        </p>
+        <h3>Manage Hours</h3>
+      </a>
     </li>
     <li>
       <h2>Contact</h2>
-      <p>
-        Manage who recieves emails from the contact us page.
-      </p>
-      <a href="/Admin/?contact=display">Manage Contact Us</a>
-    </li>
-    <li>
-      <h2></h2>
+      <a href="<?= BASEDIR ?>Admin/?contact=display">
+        <p>
+          Manage who recieves emails from the contact us page.
+        </p>
+        <h3>Manage Contact Us</h3>
+      </a>
     </li>
     <li>
       <h2>Crew Members</h2>
-      <p>
-        Manage which members are active, ban lists, and authorization levels.
-      </p>
-      <a href="/Admin/?members=display">Manage Crew Members</a>
+      <a href="<?= BASEDIR ?>Admin/?members=display">
+        <p>
+          Manage which members are active, ban lists, and authorization levels.
+        </p>
+        <h3>Manage Crew Members</h3>
+      </a>
     </li>
   </ul>
 

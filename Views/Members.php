2ec23762ff06dddf9cb83285f5f24f093d94298d
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-04-07 15:54:26 -0400

    Got members page looking nice, need to add some text

--- Views/Members.php
+++ Views/Members.php
@@ -14,20 +14,22 @@ $('.membersLink').css({'color' : '#00774B'});
 var $thisUTF8 = $('a.membersLink').find('.utf8');
 $thisUTF8.addClass("utf8Active");
 </script>
-	<div class="membersContain">
+
+
+	<div class="membersContainer">
 		<div class="contentHeader"><!--[if !IE]> -->&#10094;<!-- <![endif]-->Members<!--[if !IE]> -->&#10095;<!-- <![endif]--></div>
-		<div class="row-fluid">
-		      <th>Name</th>
-		      <th>Year</th>
-		      <th>Major</th>
-		      <th>Contact Info</th>
-		      <th>Website</th>
+
+		<p>
+			Members of the CS Crew
+		</p>
+
+		<div class="row-fluid text-center">
 		    <?php
 		    	if($none){
 		    		echo '<div class="span1">No Currently Active Members</div>';
 		    	}else{
 		    		foreach ($this->vars['members'] as $member) {
-			    		echo '<div class="span1">';
+			    		echo '<div class="span2">';
 			    			if($member['image'] != ""){
 			    				echo '<img src="'. BASEDIR . 'Views/images/profile_images/' . $member['image'] .'">'	;
 			    			}else{
@@ -35,10 +37,9 @@ $thisUTF8.addClass("utf8Active");
 			    			}
 			    			echo "</br><span>".$member['fname']." ". $member['lname']."</span><br />";
 			    			echo '<a href="mailto:'.$member['email'].'">Contact ' . $member['fname'] . '</a><br />';
-			    			echo '<a href="' . $member['url'] . '" >'.$member['url']. '</a><br />';
-
-			    			
-			    			
+			    			if($member['url']!=''){
+			    				echo '<a href="' . $member['url'] . '" >Personal Website</a><br />';	    			
+			    			}
 			    		echo '</div>';
 		    		}	
 		    	}

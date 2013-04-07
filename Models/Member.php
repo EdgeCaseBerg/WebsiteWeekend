933050627ec4c522396b89f67039d0ff37650613
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-04-07 18:41:52 -0400

    Got active and auth level administration working

--- Models/Member.php
+++ Models/Member.php
@@ -12,13 +12,36 @@ class Member{
 		$this->view = $query;
 	}
 
-	public function getMembers(){
-		// grab all of our purposes
-		$query = "SELECT fldProfileImage as image,fldFirstName as fname,fldLastName as lname,fldPersonalURL as url, fldEmail as email,fldAboutMe as aboutme FROM tblUserProfile tp, tblUserAccount ta WHERE tp.fkUserID=ta.pkUserID AND ta.active=1;";		
+	public function getMembers($start=0,$limit=10){
+		$query = "SELECT fldProfileImage as image,fldFirstName as fname,fldLastName as lname,fldPersonalURL as url, fldEmail as email,fldAboutMe as aboutme FROM tblUserProfile tp, tblUserAccount ta WHERE tp.fkUserID=ta.pkUserID AND ta.active=1 LIMIT $start , $limit;";		
 		$dbWrapper = new InteractDB();
 		$dbWrapper->customStatement($query);
 		$this->vars['info'] = $dbWrapper->returnedRows;
 		return $this->vars['info'];
+	}
+
+	public function getMembersAdmin($start=0,$limit=10){
+		$query = "SELECT pkUserID, fldFirstName as fname,fldLastName as lname, fldEmail as email, active, fldAuth as auth  FROM tblUserProfile tp, tblUserAccount ta WHERE tp.fkUserID=ta.pkUserID ORDER BY fname,lname LIMIT $start , $limit;";		
+		$dbWrapper = new InteractDB();
+		$dbWrapper->customStatement($query);
+		$this->vars['info'] = $dbWrapper->returnedRows;
+		return $this->vars['info'];
+	}
+
+	public function setMemberActive($id,$active=false){
+		$query = 'UPDATE tblUserAccount SET active ='.$active.' WHERE pkUserID = '. $id .';';
+		$dbWrapper = new InteractDB();
+		$dbWrapper->customStatement($query);
+		$this->vars['success'] = $dbWrapper->returnedRows;
+		return $this->vars['success'];	
+	}
+
+	public function setMemberAuth($id,$auth=0){
+		$query = 'UPDATE tblUserAccount SET fldAuth ='.$auth.' WHERE pkUserID = '. $id .';';
+		$dbWrapper = new InteractDB();
+		$dbWrapper->customStatement($query);
+		$this->vars['success'] = $dbWrapper->returnedRows;
+		return $this->vars['success'];		
 	}
 	
 	public function getView(){

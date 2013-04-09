c6c70f703ebb29e39a333d2b4112cfb3d81d2d22
Scott MacEwan <wsmacewan101@gmail.com>
2013-04-09 12:45:34 -0400

    Styling for article management page

--- Views/AdminViews/homeStory.php
+++ Views/AdminViews/homeStory.php
@@ -2,8 +2,14 @@
 	include 'Views/topBar.php';
 ?>
 
-
-<div class="row-fluid" id"news-management-container">
+<script type="text/javascript" src="../Views/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		var oTable= $("#manage-articles").dataTable().fnSort([[2,'desc']]);
+	});
+	
+</script>
+<div class="row-fluid" id="news-management-container">
 	<div class = "span12">
 		<div class = "rowfluid" id="top-title">
 			<div class="span1">
@@ -46,21 +52,35 @@
 		<div class="span1">
 		</div>
 		<div class = "content rowfluid">
-			<div class="span2">
+			<div class="span1">
 			</div>
-			<div class="span10">
-				<table>
-					<?php
-					foreach($this->vars['newsBundle'] as $news){
-						echo '<tr>';
-						echo '<td>'.$news->getTitle().'</td>';
-						echo '<td>'.$news->getCreatedAt().'</td>';
-						echo '<td>'.$news->getPath().'</td>';
-						echo '<td><a href="'.BASEDIR.'Admin/?news=edit&id='.$news->getId().'">Edit</a></td>';
-						echo '</tr>';
-					}
-					?>
+			<div class="span10" id="manage-table-container">
+				<table id="manage-articles">
+					<thead>
+						<tr>
+							<th width="5%">Id</th>
+							<th width="30%">Title</th>
+							<th width="22%">Creation Date</th>
+							<th width="30%">Location</th>
+							<th width="13%">Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+						foreach($this->vars['newsBundle'] as $news){
+							echo '<tr>';
+							echo '<td>'.$news->getId().'</td>';
+							echo '<td>'.$news->getTitle().'</td>';
+							echo '<td>'.$news->getCreatedAt().'</td>';
+							echo '<td>'.$news->getPath().'</td>';
+							echo '<td><a href="'.BASEDIR.'Admin/?news=edit&id='.$news->getId().'">Edit</a> | <a class="delete">Delete</a> | <a class="publish">Publish</td>';
+							echo '</tr>';
+						}
+						?>
+					</tbody>
 				</table>
+			</div>
+			<div class="span1">
 			</div>
 		</div>
 		<div class="rowfluid span12">

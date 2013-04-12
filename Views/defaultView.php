85e010c7ed89304ac50b02f693f5cb2a879022c7
JoshuaDickerson <joshuajdickerson@gmail.com>
2013-04-12 19:04:23 -0400

    got rid of old priv_level shit from the home page

--- Views/defaultView.php
+++ Views/defaultView.php
@@ -165,7 +165,7 @@ $(document).ready(function(){
   }
 
   // fire up the gallery plugin
-  var basedir = <? echo BASEDIR; ?>;
+  var basedir = <? echo "'".BASEDIR."'"; ?>;
   Galleria.loadTheme(basedir+'Views/js/galleria/themes/classic/galleria.classic.js');
   Galleria.run('#galleria');
 });

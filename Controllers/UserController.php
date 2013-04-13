294817e0a7472d06ed2da871823ca912ac980b12
JoshuaDickerson <joshuajdickerson@gmail.com>
2013-04-13 00:09:35 -0400

    documenting lost password process

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -56,11 +56,13 @@ class UserController extends AbstractController{
 						$this->view = "Profile";
 					break;
 					case "getProfile":
+						// get our profile
 						$this->vars['userProfile'] = $_SESSION['user']->getProfile();
-						// logThis($this->vars);
 					break; 
 					case "doLogin":
+						// perform a login
 						require_once "Lib/recaptchalib.php";
+						// capcha shit
 						$privatekey = "6Leq0N8SAAAAAOu25RDhsEdXFLkpCWmms2ekBuKW";
 						if ($_POST["recaptcha_response_field"]) {
 								logThis("inside post response capta");
@@ -69,42 +71,44 @@ class UserController extends AbstractController{
 								$_POST["recaptcha_challenge_field"],
 								$_POST["recaptcha_response_field"]);
 							if ($resp->is_valid) {
+								// capcha validated
 								$loginResult = $_SESSION['user']->login($this->POST['fldEmail'], $this->POST['fldPassword']);
 								if($loginResult){
-									logThis("login good");
-									header('location: '.BASEDIR.'User/?home='.$_SESSION['user']->getUserID()); 
+									// login validated
+									header('location: '.BASEDIR.'User/?home='.$_SESSION['user']->getUserID());
 									exit;
 								}else{
-									logThis("login bad");
-									// $_SESSION['notifications'] = "login Failed";
+									$_SESSION['notifications'] = "login Failed";
 									header("location: ".BASEDIR."Default/?page=login"); 
 									exit;
 								}
 							} else {
-								# set the error code so that we can display it
-								logThis("captcha bad");
-								// $_SESSION['notifications'] = "login Failed";
+								// invalid captcha
+								$_SESSION['notifications'] = "Incorrect Captcha";
 								header("location: ".BASEDIR."Default/?page=login"); 
 								exit;
 							}
 						}else{
-							# set the error code so that we can display it
-							logThis("captcha non-existent");
-							// $_SESSION['notifications'] = "login Failed";
+							// no captcha data
+							$_SESSION['notifications'] = "Please Complete the Capcha";
 							header("location: ".BASEDIR."Default/?page=login"); 
 							exit;
 						}
 					break;
 					case "lostPassword":
 						require_once "Lib/recaptchalib.php";
+						// captcha shit
 						$privatekey = "6Leq0N8SAAAAAOu25RDhsEdXFLkpCWmms2ekBuKW";
 						if ($_POST["recaptcha_response_field"]) {
+								// captcha data posted
 								$resp = recaptcha_check_answer ($privatekey,
 								$_SERVER["REMOTE_ADDR"],
 								$_POST["recaptcha_challenge_field"],
 								$_POST["recaptcha_response_field"]);
 							if ($resp->is_valid) {
+								// captcha validated
 								if(isset($_POST['fldEmail'])){
+									// perform our lost password
 									$_SESSION['user']->lostPassword($_POST['fldEmail']);
 								}else{
 									# set the error code so that we can display it
@@ -113,18 +117,52 @@ class UserController extends AbstractController{
 									exit;
 								}
 							} else {
-								# set the error code so that we can display it
+								// cpatcha failed
 								$_SESSION['notifications'] = "Captcha Invalid";
 								header("location: ".BASEDIR."Default/?page=lostPassword");
 								exit;
 							}
 						}else{
+							// no captcha data
 							$_SESSION['notifications'] = "Please complete the captcha";
 							header("location: ".BASEDIR."Default/?page=lostPassword"); 
 							exit;
 						}
 					break;
 					case "newUser":
+					require_once "Lib/recaptchalib.php";
+						$privatekey = "6Leq0N8SAAAAAOu25RDhsEdXFLkpCWmms2ekBuKW";
+						if ($_POST["recaptcha_response_field"]) {
+								// capcha data entered
+								$resp = recaptcha_check_answer ($privatekey,
+								$_SERVER["REMOTE_ADDR"],
+								$_POST["recaptcha_challenge_field"],
+								$_POST["recaptcha_response_field"]);
+							if ($resp->is_valid) {
+								// captcha validated
+								$loginResult = $_SESSION['user']->login($this->POST['fldEmail'], $this->POST['fldPassword']);
+								if($_SESSION['user']->newUser($this->POST)){
+									$this->view = "Login";
+									$_SESSION['notifications'] = "Please log into your new account";
+								}else{
+									$this->view = "Signup";
+									$_SESSION['notifications'] = "That email is already in use";
+								}
+							}else{
+								$this->view = "Signup";
+									$_SESSION['notifications'] = "captcha failed";
+							}
+						}else{
+							$this->view = "Signup";
+							$_SESSION['notifications'] = "Don't forget to enther the captcha!";
+						}
+					break;
+					case "output":
+						if($actions['output'] == "json"){
+							$this->view = "json";
+						}
+					break;
+					case "resetPassword":
 					require_once "Lib/recaptchalib.php";
 						logThis("capta");
 						$privatekey = "6Leq0N8SAAAAAOu25RDhsEdXFLkpCWmms2ekBuKW";
@@ -135,46 +173,31 @@ class UserController extends AbstractController{
 								$_POST["recaptcha_challenge_field"],
 								$_POST["recaptcha_response_field"]);
 							if ($resp->is_valid) {
-								$loginResult = $_SESSION['user']->login($this->POST['fldEmail'], $this->POST['fldPassword']);
-						    	if($_SESSION['user']->newUser($this->POST)){
-						    		$this->view = "Login";
-						    		$_SESSION['notifications'] = "Please log into your new account";
-						    	}else{
-						    		$this->view = "Signup";
-						    		$_SESSION['notifications'] = "That username is already in use";
-						    	}
-						    }else{
-						    	$this->view = "Signup";
-						    		$_SESSION['notifications'] = "captcha failed";
-						    }
-						}else{
-							$this->view = "Signup";
-						    $_SESSION['notifications'] = "Don't forget to enther the captcha!";
-						}
-					break;
-					case "output":
-						if($actions['output'] == "json"){
-							$this->view = "json";
-						}
-					break;
-					case "resetPassword":
-					// the reset password link has been clicked
-						if(isset($actions['resetPassword']) && isset($_GET['emailAddr'])){
-							// check for a row where hash and email address coincicide
-							$userHash = $actions['resetPassword'];
-							$email = $_GET['emailAddr'];
-							$array = array(
-								'tableName'=>'tblUserAccount',
-								'fldEmail'=>$email,
-								'fldLostPasswordHash'=>$userHash
-							);
-							$dbWrapper = new InteractDB('select', $array);
-							if (count($dbWrapper->returnedRows) == 1){
-								// there was a match for the hash and for the email address
-								$this->vars['lostEmail'] = $email;
-								$this->vars['lostEmailHash'] = $userHash;
-								$this->view = "resetPassword"; 
+								// the reset password link has been clicked
+								if(isset($actions['resetPassword']) && isset($_GET['emailAddr'])){
+									// check for a row where hash and email address coincicide
+									$userHash = $actions['resetPassword'];
+									$email = $_GET['emailAddr'];
+									$array = array(
+										'tableName'=>'tblUserAccount',
+										'fldEmail'=>$email,
+										'fldLostPasswordHash'=>$userHash
+									);
+									$dbWrapper = new InteractDB('select', $array);
+									if (count($dbWrapper->returnedRows) == 1){
+										// there was a match for the hash and for the email address
+										$this->vars['lostEmail'] = $email;
+										$this->vars['lostEmailHash'] = $userHash;
+										$this->view = "resetPassword"; 
+									}
+								}else{
+									// shit wasn't posted
+								}
+							}else{
+								// captcha not valid
 							}
+						}else{
+							// no capcha posted
 						}
 					break;
 					case "completeResetPassword":
